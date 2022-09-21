364dc8889ee890fccd1eb2ea89434fcaf9de2a80
Heiden <hajdenrajch.maciej@gmail.com>
2022-09-22 00:00:22 +0200

    done

---
+++ script.js
@@ -0,0 +1,31 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let color3 = document.querySelector(".color3");
+let body = document.getElementById("gradient");
+
+/*
+console.log(css);
+console.log(color1);
+console.log(color2);
+*/
+
+function setGradient() {
+    body.style.background =
+    "linear-gradient(to right, "
+     + color1.value  
+     + ","
+     + color2.value
+     + ","
+     + color3.value
+     + ")";
+
+     css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setGradient);
+
